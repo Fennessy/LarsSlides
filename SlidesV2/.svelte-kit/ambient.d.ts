62926485da2987d4b3d4dd3b946c660e9effdb17
Felix Lindblom <kingliker03@gmail.com>
2025-08-08 21:55:49 +0200

    docker frontend done

--- SlidesV2/.svelte-kit/ambient.d.ts
+++ SlidesV2/.svelte-kit/ambient.d.ts
@@ -42,7 +42,9 @@ declare module '$env/static/private' {
 	export const ComSpec: string;
 	export const DriverData: string;
 	export const EDITOR: string;
-	export const EFC_10588: string;
+	export const EFC_956: string;
+	export const FPS_BROWSER_APP_PROFILE_STRING: string;
+	export const FPS_BROWSER_USER_PROFILE_STRING: string;
 	export const FR_CUSTOMDATA_PATH: string;
 	export const GIT_ASKPASS: string;
 	export const HOME: string;
@@ -165,7 +167,9 @@ declare module '$env/dynamic/private' {
 		ComSpec: string;
 		DriverData: string;
 		EDITOR: string;
-		EFC_10588: string;
+		EFC_956: string;
+		FPS_BROWSER_APP_PROFILE_STRING: string;
+		FPS_BROWSER_USER_PROFILE_STRING: string;
 		FR_CUSTOMDATA_PATH: string;
 		GIT_ASKPASS: string;
 		HOME: string;
